8065097c262211b9277551312b8e6cbade2243d2
Mike <mmatthews@manzalab.com>
2020-01-20 10:31:13 +0100

    Added IInitializable interface to DeckbuildingManager

---
+++ Assets/Scripts/Deckbuilding/DeckbuildingManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+// class managing gameplay of Deckbuilding panel
+public class DeckbuildingManager : MonoBehaviour, IDebugable, IInitializable
+{
+	IInitializable initializableInterface => (IInitializable) this;
+	IDebugable debugableInterface => (IDebugable) this;
+
+	public bool initialized => initializableInterface.initializedInternal;
+
+	bool IInitializable.initializedInternal { get; set; }
+	string IDebugable.debugLabel => "<b>[DeckbuildingManager] : </b>";
+
+	public void Init()
+	{
+		initializableInterface.InitInternal();
+
+		Debug.Log(debugableInterface.debugLabel + "Initializing done");
+	}
+
+	void IInitializable.InitInternal()
+	{
+		initializableInterface.initializedInternal = true;
+	}
+}
