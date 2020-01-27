c63532903de25886520ae96ce29a5aa23feb8ea5
Mike <mmatthews@manzalab.com>
2020-01-27 18:05:40 +0100

    Moved project to 2019.2.18f1

---
+++ Assets/Scripts/Fight/FightManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+// class managing gameplay of Fight panel
+public class FightManager : MonoBehaviour, IDebugable, IInitializable
+{
+	IInitializable initializableInterface => (IInitializable) this;
+	IDebugable debugableInterface => (IDebugable) this;
+
+	public bool initialized => initializableInterface.initializedInternal;
+
+	bool IInitializable.initializedInternal { get; set; }
+	string IDebugable.debugLabel => "<b>[FightManager] : </b>";
+
+	public void Init()
+	{
+		initializableInterface.InitInternal();
+
+		Debug.Log(debugableInterface.debugLabel + "Initializing done");
+	}
+
+	void IInitializable.InitInternal()
+	{
+		initializableInterface.initializedInternal = true;
+	}
+}
