e80e81a74857accba4901c52f381cd572ce3b98b
Mike <mmatthews@manzalab.com>
2020-02-13 18:19:09 +0100

    Added color change, clues types storing and state checking

--- Assets/Scripts/Shogun/Path.cs
+++ Assets/Scripts/Shogun/Path.cs
@@ -1,34 +1,51 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Path : MonoBehaviour, IInitializable, IDebugable
 {
 	[Header("Settings")]
 	public float pathThickness;
+	public Color normalColor, validatedColor, wrongColor;
 
 	[Header("Debug")]
 	public Transform start;
-	public Transform end;
+	public Vector3 end;
+	public SubCategory startClue;
+	public SubCategory endClue;
 
-	RectTransform rectTransform { get { return GetComponent<RectTransform>(); } }
+	public enum State
+	{
+		NORMAL,
+		VALIDATED,
+		WRONG
+	}
+
+	public bool initialized => initializableInterface.initializedInternal;
+
+	RectTransform rectTransform => GetComponent<RectTransform>();
+	Image pathColor => GetComponent<Image>();
 
 	IDebugable debuguableInterface => (IDebugable) this;
 	IInitializable initializableInterface => (IInitializable) this;
 
-	public bool initialized => initializableInterface.initializedInternal;
-
 	bool IInitializable.initializedInternal { get; set; }
 	string IDebugable.debugLabel => "<b>[Path] : </b>";
+
+	State state;
 
 	void OnDrawGizmos()
 	{
 		if(start != null && end != null)
-			SetPath();
+			SetPath(State.NORMAL);
 	}
 
-	public void Init(Transform start, Transform end)
+	public void Init(Transform start, Vector3 end, SubCategory startClue = SubCategory.EMPTY)
 	{
 		this.start = start;
 		this.end = end;
+
+		this.startClue = startClue;
+		endClue = SubCategory.EMPTY;
 
 		initializableInterface.InitInternal();
 	}
@@ -39,9 +56,9 @@ public class Path : MonoBehaviour, IInitializable, IDebugable
 		Debug.Log(debuguableInterface.debugLabel + "Initializing done");
 	}
 
-	void SetPath()
+	public void SetPath(State newState = State.NORMAL)
 	{
-		Vector3 offset = (end.localPosition - start.localPosition);
+		Vector3 offset = (end - start.localPosition);
 
 		// moves path center
 		transform.localPosition = start.localPosition + offset / 2;
@@ -54,11 +71,65 @@ public class Path : MonoBehaviour, IInitializable, IDebugable
 		rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, pathThickness);
 
 		// set width
-		rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, Vector3.Distance(start.localPosition, end.localPosition));
+		rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, Vector3.Distance(start.localPosition, end));
 
 		// puts the path on top of hierarchy to render behind knobs
 		transform.SetAsFirstSibling();
 
-		Debug.Log(debuguableInterface.debugLabel + "Path set");
+		state = newState;
+
+		SetColorFromState();
+	}
+
+	public void SetEnd(Vector3 end, SubCategory clue = SubCategory.EMPTY)
+	{
+		this.end = end;
+		endClue = clue;
+	}
+
+	public Vector3 GetEnd()
+	{
+		return end;
+	}
+
+	// checks state of path depending on knob clues
+	public SubCategory CheckState()
+	{
+		if(startClue == endClue)
+		{
+			state = State.VALIDATED;
+			SetColorFromState();
+
+			return startClue;
+		}
+		else
+		{
+			state = State.VALIDATED;
+			SetColorFromState();
+
+			return SubCategory.EMPTY;
+		}
+
+	}
+
+	void SetColorFromState()
+	{
+		switch(state)
+		{
+			case State.NORMAL:
+				pathColor.color = normalColor;
+				break;
+			case State.VALIDATED:
+				pathColor.color = validatedColor;
+				break;
+			case State.WRONG:
+				pathColor.color = wrongColor;
+				break;
+		}
+	}
+
+	public bool ContainsKnob(Transform knob)
+	{
+		return start == knob || end == knob.position;
 	}
 }
