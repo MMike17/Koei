dadea4f5e7ddbdea899aa6e8202eb04da9e69a18
Mike <mmatthews@manzalab.com>
2020-02-13 18:15:47 +0100

    Added ClueKnob button to show clue and changed card prefab

--- Assets/Scripts/Shogun/ClueKnob.cs
+++ Assets/Scripts/Shogun/ClueKnob.cs
@@ -1,25 +1,34 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class ClueKnob : MonoBehaviour, IInitializable, IDebugable
 {
 	[Header("Assign in Inspector")]
 	public GameObject locked;
 	public GameObject unlocked, selected;
+	public Button showClueButton;
 
 	IDebugable debuguableInterface => (IDebugable) this;
 	IInitializable initializableInterface => (IInitializable) this;
 
 	public bool initialized => initializableInterface.initializedInternal;
+	public bool isUnlocked => unlocked.activeSelf;
 
 	bool IInitializable.initializedInternal { get; set; }
 	string IDebugable.debugLabel => "<b>[ClueKnob] : </b>";
 
+	Clue clue;
 	bool isSelected;
 
-	public void Init(bool isUnlocked)
+	public void Init(bool isUnlocked, Clue clue, Sprite characterPortrait, Action<string, Sprite> showClue)
 	{
 		locked.SetActive(!isUnlocked);
 		unlocked.SetActive(isUnlocked);
+
+		this.clue = clue;
+
+		showClueButton.onClick.AddListener(() => { showClue.Invoke(clue.summary, characterPortrait); });
 
 		selected.SetActive(false);
 		isSelected = false;
@@ -34,13 +43,18 @@ public class ClueKnob : MonoBehaviour, IInitializable, IDebugable
 	}
 
 	// called when you hover with mouse and you started a path
-	public void Select()
+	public void SelectForPath()
 	{
 		isSelected = !isSelected;
 
 		selected.SetActive(isSelected);
 		unlocked.SetActive(!isSelected);
 
-		Debug.Log(debuguableInterface.debugLabel + "Selected knob");
+		Debug.Log(debuguableInterface.debugLabel + (isSelected? "Selected knob": "Unselected knob"));
+	}
+
+	public SubCategory GetSubCategory()
+	{
+		return clue.correctedSubCategory;
 	}
 }
