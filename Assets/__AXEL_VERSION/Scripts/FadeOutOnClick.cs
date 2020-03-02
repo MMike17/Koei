2ed6bc9c86fbfdd0410b41a1a3cc61b7bb2a4e25
Axel <axelrouet.job@gmail.com>
2020-03-02 11:53:54 +0100

    Fade attack menu to end

---
+++ Assets/__AXEL_VERSION/Scripts/FadeOutOnClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOutOnClick : MonoBehaviour
+{
+    private AttackSystem attack;
+
+    private void Start()
+    {
+        attack = FindObjectOfType<AttackSystem>();
+    }
+
+    public void OnClickButton()
+    {
+        Debug.Log("Prepare to fade buttons", gameObject);
+        attack.OnClick();
+    }
+
+}
--- Assets/__AXEL_VERSION/Scripts/NewBattleSys/AttackSystem.cs
+++ Assets/__AXEL_VERSION/Scripts/NewBattleSys/AttackSystem.cs
@@ -10,11 +10,15 @@ public class AttackSystem : MonoBehaviour
     public GameObject buttonsToMakeAppear;
 
     public int currentStep;
+    public string animVariable = "PlayFadeOut";
 
+    // Animation system
+    private List<Animator> anims = new List<Animator>();
+    
     private GameObject mainParent;
     private List<Button> buttonsToClick = new List<Button>();
 
-    private bool alreadyAddedStep;
+    private bool alreadyLaunchedAnimation;
 
     private void OnEnable()
     {
@@ -31,14 +35,36 @@ public class AttackSystem : MonoBehaviour
             {
                 // Next phase
                 currentStep++;
-                Debug.Log("Next Step - FIGHT GAMEPLAY + CURRENTSTEP >> " + currentStep);
-                InitializeButtons();
+
+                if (currentStep < stepsOfBattle.Length)
+                {
+                    InitializeButtons();
+                }
+                else
+                {
+                    if (!alreadyLaunchedAnimation)
+                    {
+                        mainParent.GetComponentInChildren<Animator>().SetBool(animVariable, true);
+                        Debug.Log(mainParent.GetComponentInChildren<Animator>().name);
+                        mainParent.GetComponent<Animator>().SetBool(animVariable, true);
+                        alreadyLaunchedAnimation = true;
+                    }
+                }
             }
         }
+
+        // Set active false the panel if panel is invisible
+        /*
+        if (mainParent.GetComponent<Image>().color == new Color(mainParent.GetComponent<Image>().color.r, mainParent.GetComponent<Image>().color.g, mainParent.GetComponent<Image>().color.b, 0))
+        {
+            mainParent.SetActive(false);
+        }
+        */
     }
 
     private void InitializeButtons()
     {
+        anims.Clear();
         Debug.Log("Destroying buttons...");
         buttonsToClick.Clear();
         for (int i = 0; i < GameObject.FindGameObjectsWithTag("Fight/Fight Button").Length; i++)
@@ -56,10 +82,23 @@ public class AttackSystem : MonoBehaviour
         {
             // Create new buttons
             GameObject newButton = Instantiate(buttonsToMakeAppear);
-            newButton.transform.parent = mainParent.transform;
+            newButton.transform.SetParent(mainParent.transform);
+            Debug.Log(newButton.name + "is initializing now.", newButton);
             buttonsToClick.Add(newButton.GetComponent<Button>());
             newButton.GetComponentInChildren<TextMeshProUGUI>().text = stepsOfBattle[currentStep].buttonSentences[buttons];
+            
+            anims.Add(newButton.GetComponent<Animator>());
+            anims.Add(newButton.GetComponentInChildren<Animator>());
+
             Debug.Log("Buttons Initialized with success");
+        }
+    }
+
+    public void OnClick()
+    {
+        for (int i = 0; i < anims.Count; i++)
+        {
+            anims[i].SetBool("Play", true);
         }
     }
 }
