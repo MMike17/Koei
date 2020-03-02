931ef7d440f3c2ef8fa0bfbf750cddcf71a9333f
Axel <axelrouet.job@gmail.com>
2020-03-02 09:01:39 +0100

    Started Fight Cinematic System
    Boolean Based

---
+++ Assets/__AXEL_VERSION/Scripts/Cinematics/CineDelay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CineDelay : MonoBehaviour
+{
+    public Animator animator;
+
+    public string variableAnim;
+
+    public float startDelay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("PlayAnim", startDelay);
+    }
+
+    private void PlayAnim()
+    {
+        animator.SetBool(variableAnim, true);
+    }
+}
---
+++ Assets/__AXEL_VERSION/Scripts/Cinematics/CineStart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CineStart : MonoBehaviour
+{
+    public Animator animator;
+    public string variableAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator.SetBool(variableAnim, true);
+    }
+}
