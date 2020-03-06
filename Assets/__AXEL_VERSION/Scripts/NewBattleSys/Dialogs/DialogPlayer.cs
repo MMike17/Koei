1f9c68184c7c645edbf532a1457d85d8346e0a68
Axel <axelrouet.job@gmail.com>
2020-03-06 09:56:56 +0100

    Added Fight Cinematic Intro

---
+++ Assets/__AXEL_VERSION/Scripts/NewBattleSys/Dialogs/DialogPlayer.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogPlayer : MonoBehaviour
+{
+    public DialogsObj dialog;
+    public float delayStart;
+    public float delayBetweenSentences;
+
+    public float delayBetweenAnswers = 1.75f;
+
+    [Space(25)]
+
+    public TextMeshProUGUI textPlayer;
+    public TextMeshProUGUI textEnemy;
+
+    [Space(25)]
+
+    public bool isPlayerStarts;
+
+
+    private int dialogNumber;
+    private bool hasCoroutined;
+
+    private void Start()
+    {
+        // Set dialogs to zero
+        textPlayer.text = "";
+        textEnemy.text = "";
+
+        InvokeRepeating("DisplayDialog", delayStart, delayBetweenSentences);
+    }
+
+    public void DisplayDialog()
+    {
+        if(dialogNumber < dialog.dialogsPlayer.Length || dialogNumber < dialog.dialogsEnemy.Length)
+        {
+            isPlayerStarts = !isPlayerStarts;
+
+            if (!hasCoroutined)
+            {
+                StartCoroutine(WaitDelay());
+                hasCoroutined = true;
+            }
+        }
+        if(dialogNumber == dialog.dialogsPlayer.Length ||dialogNumber == dialog.dialogsEnemy.Length)
+        {
+            // Set dialogs to zero
+            textPlayer.text = "";
+            textEnemy.text = "";
+
+            CancelInvoke("DisplayDialog");
+        }
+    }
+
+    IEnumerator WaitDelay()
+    {
+        // Say the sentence
+        if (isPlayerStarts)
+        {
+            textEnemy.text = dialog.dialogsEnemy[dialogNumber];
+            textPlayer.text = "";
+        }
+        else
+        {
+            textPlayer.text = dialog.dialogsPlayer[dialogNumber];
+            textEnemy.text = "";
+        }
+
+        yield return new WaitForSeconds(delayBetweenAnswers);
+
+        // Say the reversed sentence
+        if (isPlayerStarts)
+        {
+            textPlayer.text = dialog.dialogsPlayer[dialogNumber];
+            textEnemy.text = "";
+        }
+        else
+        {
+            textPlayer.text = "";
+            textEnemy.text = dialog.dialogsEnemy[dialogNumber];
+
+        }
+
+        isPlayerStarts = !isPlayerStarts;
+
+        Debug.Log("<b>START DIALOG >> </b> Phasis is finished");
+
+        dialogNumber++;
+        hasCoroutined = false;
+    }
+}
---
+++ Assets/__AXEL_VERSION/Scripts/ScriptOBJ/DialogsObj.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dialog", menuName = "Koei/Battle/Dialog")]
+public class DialogsObj : ScriptableObject
+{
+    [Header("- - - PLAYER - - -")]
+    [TextArea]
+    public string[] dialogsPlayer;
+
+    [Header("- - - ENEMY - - -")]
+    [TextArea]
+    public string[] dialogsEnemy;
+}
