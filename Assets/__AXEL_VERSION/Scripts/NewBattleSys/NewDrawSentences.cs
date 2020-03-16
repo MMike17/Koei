e731343a36e1e424e345922e4367f929e291d9f7
Axel <axelrouet.job@gmail.com>
2020-03-16 10:25:50 +0100

    Almost Fixed. Almost.

--- Assets/__AXEL_VERSION/Scripts/NewBattleSys/NewDrawSentences.cs
+++ Assets/__AXEL_VERSION/Scripts/NewBattleSys/NewDrawSentences.cs
@@ -16,54 +16,52 @@ public class NewDrawSentences : MonoBehaviour
 
     private List<GameObject> bttAttacks = new List<GameObject>();
 
+    /*
     private void Start()
     {
         DrawSentencesWar();
     }
+    */
 
     public void DrawSentencesWar()
     {
-        CheckAndDestroyOldButtons();
         DrawSentences(war, EditorPrefs.GetInt("totalDialogs_" + war.cat.ToString()));
     }
     public void DrawSentencesFamily()
     {
-        CheckAndDestroyOldButtons();
         DrawSentences(family, EditorPrefs.GetInt("totalDialogs_" + family.cat.ToString()));
     }
     public void DrawSentencesMoney()
     {
-        CheckAndDestroyOldButtons();
         DrawSentences(money, EditorPrefs.GetInt("totalDialogs_" + money.cat.ToString()));
     }
     public void DrawSentencesReligion()
     {
-        CheckAndDestroyOldButtons();
         DrawSentences(religion, EditorPrefs.GetInt("totalDialogs_" + religion.cat.ToString()));
     }
 
     private void DrawSentences(AttackObj attack, int buttonsToDisplay)
     {
+        bttAttacks.AddRange(GameObject.FindGameObjectsWithTag("Fight/Fight Button"));
+        Debug.Log(bttAttacks);
+        if (parent.childCount != 0)
+        {
+            Debug.Log("Buttons to remove (number only): " + parent.childCount + " | Buttons to display: " + buttonsToDisplay);
+
+            for (int btt = 0; btt <= bttAttacks.Count; btt++)
+            {
+                Destroy(bttAttacks[btt]);
+                Debug.Log(btt);
+                //Destroy(parent.GetChild(0).gameObject);
+            }
+        }
+
         for (int i = 0; i < buttonsToDisplay; i++)
         {
             GameObject button = Instantiate(buttonPrefab);
             button.transform.SetParent(parent, false);
             button.transform.localScale = Vector3.one;
             button.GetComponentInChildren<TextMeshProUGUI>().text = attack.buttonSentences[i];
-        }
-    }
-
-    private void CheckAndDestroyOldButtons()
-    {
-        if(GameObject.FindGameObjectsWithTag("Fight/Fight Button").Length > 0)
-        {
-            bttAttacks.AddRange(GameObject.FindGameObjectsWithTag("Fight/Fight Button"));
-
-            for (int btt = 0; btt <= bttAttacks.Count; btt++)
-            {
-                Destroy(bttAttacks[btt]);
-            }
-            bttAttacks.Clear();
         }
 
     }
