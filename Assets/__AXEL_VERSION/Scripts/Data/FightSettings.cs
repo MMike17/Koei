e59c07edd7c41effdfdd837abbc50b97544e2ad5
Mike <mmatthews@manzalab.com>
2020-04-06 09:29:17 +0200

    Deleted deprecated stuff

--- Assets/__AXEL_VERSION/Scripts/Data/FightSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class FightSettings : MonoBehaviour
-{
-    public static int currentHpEnemy = 3;
-    public static int currentHpPlayer;
-}
